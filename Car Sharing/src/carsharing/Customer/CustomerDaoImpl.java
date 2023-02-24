bdf575567732dd858c80d4f857fd9c5d3004c0aa
KubaBili001 <kubabili.wawa@gmail.com>
2023-02-24 23:27:48 +0100

    small changes

--- Car Sharing/src/carsharing/Customer/CustomerDaoImpl.java
+++ Car Sharing/src/carsharing/Customer/CustomerDaoImpl.java
@@ -1,4 +1,61 @@
 package carsharing.Customer;
 
+import carsharing.Company.Company;
+import carsharing.Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 public class CustomerDaoImpl {
+
+    ArrayList<Customer> customers = new ArrayList<>();
+
+    private static final Connection connection = Database.getConnection();
+    private static final String listAllQuery = "SELECT * FROM CUSTOMER ORDER BY ID ASC";
+    private static final String addNewRecord = "INSERT INTO CUSTOMER (NAME,RENTED_CAR_ID) VALUES(?,NULL)";
+    private static final String updateRecord = "UPDATE CUSTOMER SET RENTED_CAR_ID = (SELECT ID FROM CAR WHERE COMPANY_ID = ?) WHERE ID = ?";
+
+    public static ArrayList<Customer> listAll() {
+        ArrayList<Customer> result = new ArrayList<>();
+
+        try (PreparedStatement statement = connection.prepareStatement(listAllQuery)) {
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                int ID = resultSet.getInt("ID");
+                String NAME = resultSet.getString("NAME");
+                result.add(new Customer(ID,NAME));
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void addNewRecord(String NAME) {
+        try (PreparedStatement statement = connection.prepareStatement(addNewRecord)) {
+
+            statement.setString(1, NAME);
+            statement.executeUpdate();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void updateRecord(int CUSTOMER_ID, int COMPANY_ID) {
+        try (PreparedStatement statement = connection.prepareStatement(addNewRecord)) {
+
+            statement.setString(1, String.valueOf(COMPANY_ID));
+            statement.setString(2, String.valueOf(CUSTOMER_ID));
+            statement.executeUpdate();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- Car Sharing/src/carsharing/Menu.java
+++ Car Sharing/src/carsharing/Menu.java
@@ -5,27 +5,28 @@ import carsharing.Car.Car;
 import carsharing.Car.CarDaoImpl;
 import carsharing.Company.Company;
 import carsharing.Company.CompanyDaoImpl;
+import carsharing.Customer.Customer;
+import carsharing.Customer.CustomerDaoImpl;
+
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Menu {
+
     private static final Scanner scanner = new Scanner(System.in);
     private static boolean isFinished = false;
     private static int choice;
 
     public static void initUI() {
         while (!isFinished) {
-            log("1. Log in as a manager\n0. Exit");
+            log("1. Log in as a manager\n2. Log in as a customer\n3. Create a customer\n0. Exit");
 
+            choice();
 
-            /*2. Log in as a customer
-            3. Create a customer*/
-
-            choice = scanner.nextInt();
             switch (choice) {
                 case 1 -> initSubMenu();
-                //case 2 ->
-                //case 3 ->
+                case 2 -> listCustomers();
+                case 3 -> createCustomer();
                 case 0 -> isFinished = true;
                 default -> log("wrong input, try again\n");
             }
@@ -38,7 +39,8 @@ public class Menu {
         while(!finish) {
             log("1. Company list\n2. Create a company\n0. Back");
 
-            choice = scanner.nextInt();
+            choice();
+
             switch (choice) {
                 case 1 -> listCompanies();
                 case 2 -> createCompany();
@@ -51,17 +53,34 @@ public class Menu {
     public static boolean initCompanyMenu(int COMPANY) {
         boolean finish = false;
 
-        while (!finish) {
+        while (true) {
 
             log("1. Car list");
             log("2. Create a car");
             log("0. Back");
 
-            choice = scanner.nextInt();
+            choice();
 
             switch (choice) {
                 case 1 -> listCars(COMPANY);
                 case 2 -> createCar(COMPANY);
+                case 0 -> {
+                    return true;
+                }
+            }
+        }
+    }
+
+    public static boolean initCustomerMenu(int CUSTOMER_ID) {
+        while (true) {
+
+            log("1. Rent a car\n2. Return a rented car\n3. My rented car\n0. Back");
+
+            choice();
+
+            switch (choice) {
+                case 1 -> chooseCompany(CUSTOMER_ID);
+                //case 2 ->
                 case 0 -> {
                     return true;
                 }
@@ -82,15 +101,43 @@ public class Menu {
             while (!finish) {
 
                 log("Choose the company:");
-                companyList.forEach(company -> System.out.println(company.toString()));
+                AtomicInteger counter = new AtomicInteger(0);
+                companyList.forEach(company -> System.out.println(counter.incrementAndGet() + ". " + company.getName()));
                 log("0. Back");
 
-                choice = scanner.nextInt();
+                choice();
 
                 if (choice == 0) {
                     finish = true;
                 } else {
                     finish = initCompanyMenu(choice);
+                }
+            }
+        }
+    }
+
+    public static void listCustomers() {
+        ArrayList<Customer> customerList = CustomerDaoImpl.listAll();
+
+        boolean finish = false;
+
+        if(customerList.isEmpty()) {
+            log("The customer list is empty!\n");
+        } else {
+
+            while (!finish) {
+
+                log("Choose the customer:");
+                AtomicInteger counter = new AtomicInteger(0);
+                customerList.forEach(customer -> System.out.println(counter.incrementAndGet() + ". " + customer.getName()));
+                log("0. Back");
+
+                choice();
+
+                if (choice == 0) {
+                    finish = true;
+                } else {
+                    finish = initCustomerMenu(choice);
                 }
             }
         }
@@ -104,11 +151,14 @@ public class Menu {
         } else {
             AtomicInteger counter = new AtomicInteger(0);
             carList.forEach(car -> System.out.println(counter.incrementAndGet() + ". " + car.getName()));
+            log("");
         }
     }
 
     public static void createCompany() {
         log("Enter the company name:");
+
+        System.out.print("> ");
 
         Scanner scanner = new Scanner(System.in);
         String NAME = scanner.nextLine();
@@ -119,12 +169,75 @@ public class Menu {
     public static void createCar(int COMPANY) {
         log("Enter the car name:");
 
+        System.out.print("> ");
+
         Scanner scanner = new Scanner(System.in);
         String NAME = scanner.nextLine();
 
         CarDaoImpl.addNewRecord(NAME,COMPANY);
     }
 
+    public static void createCustomer() {
+        log("Enter the customer name:");
+
+        System.out.print("> ");
+
+        Scanner scanner = new Scanner(System.in);
+        String NAME = scanner.nextLine();
+
+        CustomerDaoImpl.addNewRecord(NAME);
+    }
+
+    public static void choice() {
+        System.out.print("> ");
+        choice = scanner.nextInt();
+        System.out.println();
+    }
+
+    public static void chooseCompany(int CUSTOMER_ID) {
+        ArrayList<Company> companyList = CompanyDaoImpl.listAll();
+        boolean finish = false;
+
+        if(companyList.isEmpty()) {
+            log("The company list is empty!\n");
+        } else {
+
+            while (!finish) {
+
+                log("Choose the company:");
+                AtomicInteger counter = new AtomicInteger(0);
+                companyList.forEach(company -> System.out.println(counter.incrementAndGet() + ". " + company.getName()));
+                log("0. Back");
+
+                choice();
+
+                if (choice == 0) {
+                    finish = true;
+                } else {
+                    finish = chooseCar(CUSTOMER_ID, choice);
+                }
+            }
+        }
+    }
+
+    public static boolean chooseCar(int CUSTOMER_ID, int COMPANY_ID) {
+        ArrayList<Car> carList = CarDaoImpl.listCompanyCars(COMPANY_ID);
+
+        if(carList.isEmpty()) {
+            log("The car list is empty!\n");
+            return true;
+        } else {
+            AtomicInteger counter = new AtomicInteger(0);
+            carList.forEach(car -> System.out.println(counter.incrementAndGet() + ". " + car.getName()));
+            log("");
+
+            choice();
+
+            CustomerDaoImpl.updateRecord(CUSTOMER_ID,COMPANY_ID);
+        }
+
+        return false;
+    }
 
     public static void log(String msg) {
         System.out.println(msg);
